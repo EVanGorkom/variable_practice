fa10966764cbe71c2ff84ae625c4886420c283dc
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-06-18 16:54:22 -0500

    Reassign the original string values and add puts statements

--- variables.rb
+++ variables.rb
@@ -15,3 +15,10 @@ rdm_num3 = 50.55
 #Boolean examples
 potatoes_are_tasty = true
 pickles_are_tasty = false
+
+#Reassigning practice
+string_example = "This is a NEW string example that reassigns my old value to this one."
+name = "My name is now Ethan the White, you knew me previously as Ethan the Grey"
+
+puts string_example
+puts name
